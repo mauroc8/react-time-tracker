41509ac854b9bf59239b1e4be5497565ede91dbf
Mauro Cano <maurocanobrusa@gmail.com>
2019-12-04 14:57:45 -0300

    Updates

---
+++ precache-manifest.f657fbfb0f3872f1728e38bba6fc379d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2daf2acb6ea46dbca6ce789f4e6cdf9a",
+    "url": "/react-time-tracker/index.html"
+  },
+  {
+    "revision": "539d33685430a90acdea",
+    "url": "/react-time-tracker/static/css/main.0bab40ce.chunk.css"
+  },
+  {
+    "revision": "3fdc2a0ac35c312671ce",
+    "url": "/react-time-tracker/static/js/2.0602d3f3.chunk.js"
+  },
+  {
+    "revision": "539d33685430a90acdea",
+    "url": "/react-time-tracker/static/js/main.6c655fcd.chunk.js"
+  },
+  {
+    "revision": "a48ec5560c2cc21fff07",
+    "url": "/react-time-tracker/static/js/runtime-main.0caad49a.js"
+  }
+]);
