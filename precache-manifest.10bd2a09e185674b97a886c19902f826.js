9c73e9c97459b00fbff4b1589c2aac62227cd316
Mauro Cano <maurocanobrusa@gmail.com>
2019-12-05 16:03:40 -0300

    Updates

--- precache-manifest.10bd2a09e185674b97a886c19902f826.js
+++ precache-manifest.10bd2a09e185674b97a886c19902f826.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f4c44e420c482c80576d86f2724638be",
+    "revision": "4e7ff8af1b6621028a6be333042e339b",
     "url": "/react-time-tracker/index.html"
   },
   {
-    "revision": "2b1b1d3709f6f45d7648",
-    "url": "/react-time-tracker/static/css/main.1ffaec7f.chunk.css"
+    "revision": "c008f2ff642cc6ce7247",
+    "url": "/react-time-tracker/static/css/main.e960fb29.chunk.css"
   },
   {
     "revision": "9f58a84b94f7ca305561",
     "url": "/react-time-tracker/static/js/2.6c1179ed.chunk.js"
   },
   {
-    "revision": "2b1b1d3709f6f45d7648",
-    "url": "/react-time-tracker/static/js/main.780f6935.chunk.js"
+    "revision": "c008f2ff642cc6ce7247",
+    "url": "/react-time-tracker/static/js/main.5946dcba.chunk.js"
   },
   {
     "revision": "a48ec5560c2cc21fff07",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-time-tracker/precache-manifest.9482a25d95b8d4d84f42d798ef5570c4.js"
+  "/react-time-tracker/precache-manifest.10bd2a09e185674b97a886c19902f826.js"
 );
 
 self.addEventListener('message', (event) => {
